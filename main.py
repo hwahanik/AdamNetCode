19ee79283773d031c119813cba4c2930f27d8f2e
Helmut Wahanik <hwahanik@Helmuts-MacBook-Pro.local>
2018-11-11 17:18:26 -0500

    Rename project

--- main.py
+++ main.py
@@ -6,37 +6,47 @@
 # -The optimal architecture of the network, which is variable during runtime.
 # -A set of "ensemble" weights that minimizes the objective function.
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 
-import trainAndEvaluate
-import tensorflow as tf
+# Variables that define the adaptive architecture
+#
+# LEARN_MIXTURE_WEIGHTS # @param {type:"boolean"}
+# Use variable weights that appear in linear combination of subnetworks, for
+# optimization of the mixture of subnetworks.
+
+# ADANET_LAMBDA         # @param {type:"number"}
+# lambda parameter serves to prevent the optimization from assigning too
+# much weight to more complex subnetworks
+
+# BOOSTING_ITERATIONS   # @param {type:"integer"}
+# Size of adaptive architecture of AdaNet.
+
+
 import matplotlib.pyplot as plt
-from matplotlib import interactive
-interactive(True)
-
-(x_train, y_train), (x_test, y_test) = (
-    tf.keras.datasets.boston_housing.load_data())
+import tensorflow as tf
+import trainAndEvaluate
 
 # @title AdaNet parameters
 LEARNING_RATE = 0.001   # @param {type:"number"}
-TRAIN_STEPS = 1000      # @param {type:"integer"} 100000
+TRAIN_STEPS = 10000     # @param {type:"integer"} 100000
 BATCH_SIZE = 32         # @param {type:"integer"}
+SIZE_ADAPTIVE = 6       # @param {type:"integer"} Network depth allowed to adapt.
+
+# Testing the AdaNet architecture for the Boston Housing Dataset.
+# 1. Load data
+(x_train, y_train), (x_test, y_test) = (
+    tf.keras.datasets.boston_housing.load_data())
 
 
-# Use variable weights that appear in linear combination of subnetworks.
-# LEARN_MIXTURE_WEIGHTS # @param {type:"boolean"}
+# 2. Training and evaluating the model.
 
-# lambda parameter serves to prevent the optimization from assigning too
-# much weight to more complex subnetworks
-# ADANET_LAMBDA         # @param {type:"number"}
-
-# Size of adaptive architecture of AdamNet.
-# BOOSTING_ITERATIONS = 5  # @param {type:"integer"}
+# 2. a. -Train with no optimization of the mixture weights.
+#       -Lambda=0;
+#       -Increasing size of the different networks.
+#       -Constant learning rate, and constant batch size.
 
 loss_results = []
-for i in range(1, 4):
+architectures = list(range(1, SIZE_ADAPTIVE+1))
+for i in architectures:
     results, _ = trainAndEvaluate.train_and_evaluate(x_train, y_train, x_test, y_test,
                                                      LEARNING_RATE,
                                                      TRAIN_STEPS,
@@ -49,5 +59,16 @@ for i in range(1, 4):
     print("Loss:", results["average_loss"])
     print("Architecture:", trainAndEvaluate.ensemble_architecture(results))
 
-plt.scatter(list(range(1, 4)), loss_results)
+plt.ioff()
+fig, ax = plt.subplots()
+ax.scatter(architectures, loss_results, c="b", alpha=0.5)
+x_int_range = range(min(architectures), max(architectures)+1)
+for i, txt in enumerate(loss_results):
+    ax.annotate(txt, [architectures[i], loss_results[i]])
+plt.title("AdamNet training")
+plt.xlabel("Network depth")
+plt.ylabel("MSE (loss)")
+plt.xticks(x_int_range)
+plt.ylim([0, max(loss_results)])
+plt.draw()
 plt.show()
