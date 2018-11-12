f2ea284680b1b059a9694dd52335dfe7953d3e19
Helmut Wahanik <hwahanik@Helmuts-MacBook-Pro.local>
2018-11-12 11:54:24 -0500

    Experiments with regularization parameter

--- main.py
+++ main.py
@@ -20,22 +20,21 @@
 # BOOSTING_ITERATIONS   # @param {type:"integer"}
 # Size of adaptive architecture of AdaNet.
 
-
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import trainAndEvaluate
+import numpy as np
 
 # @title AdaNet parameters
-LEARNING_RATE = 0.001   # @param {type:"number"}
-TRAIN_STEPS = 10000     # @param {type:"integer"} 100000
-BATCH_SIZE = 32         # @param {type:"integer"}
-SIZE_ADAPTIVE = 6       # @param {type:"integer"} Network depth allowed to adapt.
+LEARNING_RATE = 0.001       # @param {type:"number"}
+TRAIN_STEPS = 10000         # @param {type:"integer"} 100000
+BATCH_SIZE = 32             # @param {type:"integer"}
+BOOSTING_ITERATIONS = 5     # @param {type:"integer"} Boosting iterations ==> Network depth.
 
 # Testing the AdaNet architecture for the Boston Housing Dataset.
 # 1. Load data
 (x_train, y_train), (x_test, y_test) = (
     tf.keras.datasets.boston_housing.load_data())
-
 
 # 2. Training and evaluating the model.
 
@@ -44,31 +43,69 @@ SIZE_ADAPTIVE = 6       # @param {type:"integer"} Network depth allowed to adapt
 #       -Increasing size of the different networks.
 #       -Constant learning rate, and constant batch size.
 
-loss_results = []
-architectures = list(range(1, SIZE_ADAPTIVE+1))
-for i in architectures:
-    results, _ = trainAndEvaluate.train_and_evaluate(x_train, y_train, x_test, y_test,
+# Experiments
+
+
+def training_results(x_tr, y_tr, x_tst, y_tst, a_lambda, learn_mixtures):
+    results, _ = trainAndEvaluate.train_and_evaluate(x_tr, y_tr, x_tst, y_tst,
                                                      LEARNING_RATE,
                                                      TRAIN_STEPS,
                                                      BATCH_SIZE,
-                                                     learn_mixture_weights=False,
-                                                     adanet_lambda=0,
-                                                     boosting_iterations=i)
-    loss_results.append(results["average_loss"])
-    print(loss_results)
-    print("Loss:", results["average_loss"])
+                                                     learn_mixtures,
+                                                     a_lambda,
+                                                     boosting_iterations=BOOSTING_ITERATIONS)
     print("Architecture:", trainAndEvaluate.ensemble_architecture(results))
+    print(results["average_loss"])
+    print("Uniform average loss:", results["average_loss/adanet/uniform_average_ensemble"])
+    print("Adanet lambda", a_lambda)
+    return [results["average_loss"], results["average_loss/adanet/uniform_average_ensemble"]]
+
+
+# Training without learning mixtures of deep networks, learn_mixtures = false
+# lambda = [0.1, 0.15, 0.2, 0.25 ... 1]
+
+loss_results = []
+uniform_loss_results = []
+adanet_lambda = np.linspace(0, 0.2, 11)
+learnMixtures = True
+
+# Training the ensemble, and calculating losses
+for l in adanet_lambda:
+    loss = training_results(x_train, y_train, x_test, y_test, l, learnMixtures)[0]
+    uniform_loss = training_results(x_train, y_train, x_test, y_test, l, learnMixtures)[1]
+    loss = float("{0:.2f}".format(loss))
+    uniform_loss_results.append(uniform_loss)
+    loss_results.append(loss)
+#
+
+print(adanet_lambda)
+print(loss_results)
+print(uniform_loss_results)
+
+
+# Plotting results
 
 plt.ioff()
+epsilon = 1e-2
+
 fig, ax = plt.subplots()
-ax.scatter(architectures, loss_results, c="b", alpha=0.5)
-x_int_range = range(min(architectures), max(architectures)+1)
+ax.scatter(adanet_lambda, loss_results, color="blue", alpha=0.5)
+ax.scatter(adanet_lambda, uniform_loss_results, c="green", alpha=0.5, )
+
 for i, txt in enumerate(loss_results):
-    ax.annotate(txt, [architectures[i], loss_results[i]])
-plt.title("AdamNet training")
-plt.xlabel("Network depth")
-plt.ylabel("MSE (loss)")
-plt.xticks(x_int_range)
-plt.ylim([0, max(loss_results)])
+    ax.annotate(txt, (adanet_lambda[i], loss_results[i]))
+
+title = "AdaNet ensemble: uniform (black) vs regularized (red), for depth = 5"
+ax.plot(adanet_lambda, loss_results, color="red")
+
+ax.plot(adanet_lambda, uniform_loss_results, color="black")
+l_range = np.linspace(0, 1, 6)
+
+plt.title(title, color="b", size="medium")
+plt.xlabel("regularization parameter", color="b", size="medium")
+plt.ylabel("MSE (loss)", color="b", size="medium")
+plt.xticks(l_range)
+
+plt.ylim([0.045, 0.065])
 plt.draw()
 plt.show()
